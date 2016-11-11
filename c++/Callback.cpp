fe52966ad34a82c3181eca987c9ae06d9e41ee09
xuzebin <zebinxu7@gmail.com>
2016-11-11 17:33:10 -0500

    * Callback usage in C++

---
+++ c++/Callback.cpp
@@ -0,0 +1,45 @@
+/**
+ * A simple example showing how to use callback in C++.
+ */
+
+#include <string>
+#include <iostream>
+
+class CallbackInterface {
+public:
+  virtual void callback_func(std::string something) = 0;
+};
+
+
+class Caller {
+private:
+  CallbackInterface* callback;
+
+public:
+  void addCallback(CallbackInterface* callback) {
+    this->callback = callback;
+  }
+
+  void testCallback() {
+    if (callback != NULL) {
+      callback->callback_func("callback is inovked!");
+    }
+  }
+};
+
+class Callee : public CallbackInterface {
+public:
+  void callback_func(std::string something) {
+    std::cout << something << std::endl;
+  }
+};
+
+
+
+int main() {
+  Caller caller;
+  Callee callee;
+  caller.addCallback(&callee);
+  caller.testCallback();
+  return 0;
+}
